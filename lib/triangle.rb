707585fd76d0c5ce2af45639eed2681db0819944
Jazmin M <jazminmerckel@gmail.com>
2019-10-31 15:18:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,14 @@
 class Triangle
   
-  attr_accessor :kind
+  attr_accessor :side_1, :side_2, :side_3
   
   def initialize(side_1, side_2, side_3)
+    if side_1 <= 0 || side_2 <=0 || side_3 <=0
+      raise TriangleError
+    end
+    if side_1 + side_2 >= side_3
+      raise TriangleError
+    end
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
@@ -13,9 +19,13 @@ class Triangle
   end
     
   def kind  
-    
-    
-    
+    if side_1 == side_2 && side_2 == side_3
+      return :equilateral
+    elsif side_1 == side_2 || side_2 == side_3 || side_1 = side_3
+      return :isosceles
+    else 
+      return :scalene
+    end 
   end
   
   
