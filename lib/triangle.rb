fbdbb2a5af7cb40243d6aecff1e08bde591fb3f3
Jazmin M <jazminmerckel@gmail.com>
2019-10-31 15:27:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
     if side_1 <= 0 || side_2 <=0 || side_3 <=0
       raise TriangleError
     end
-    if side_1 + side_2 <= side_3
+    if side_1 + side_2 <= side_3 || side_1 + side_3 <= side_2
       raise TriangleError
     end
     @side_1 = side_1
